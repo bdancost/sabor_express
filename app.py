013c8f014a0fcfcd20183673651f56e0c415ec7c
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2024-11-05 21:45:44 -0300

    Adicionando funçao para organizar o código

--- app.py
+++ app.py
@@ -1,9 +1,43 @@
-print('Sabor Express\n')
+import os
+
+print("""
+░██████╗░█████╗░██████╗░░█████╗░██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝
+
+███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+█████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
+     
+       """)
 
 print('1. Cadastrar Restaurante')
 print('2. Listar Restaurante')
 print('3. Ativar Restaurante')
 print('4. Sair\n')
 
-opcao_escolhida = input('Digite a opção desejada: ')
-print('Você escolheu a opção', opcao_escolhida)
+opcao_escolhida = int(input('Digite a opção desejada: '))
+print(f'Você escolheu a opção, {opcao_escolhida}')
+
+def finalizar_app():
+  os.system('clear')
+  print('Finalizando o app')
+
+if opcao_escolhida == 1:
+  print('Cadastrando Restaurante')
+
+elif opcao_escolhida == 2:
+  print('Listando Restaurante')
+
+elif opcao_escolhida == 3:
+  print('Ativando Restaurante')
+
+else:
+  finalizar_app()  
+    
