dfcaf21188287bf56b71cd5d09514a04b82e4988
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2024-11-07 18:31:04 -0300

    Adicionando novas funcionalidades

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 
+restaurantes = ['Pizzas', 'Bebidas', 'Sobremesas']
+
 def exibir_nome_do_progrma():
   print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░
@@ -27,24 +29,55 @@ def exibir_opcoes():
 def finalizar_app():
   os.system('clear')
   print('Finalizando o app')
+def opcao_invalida():
+  print('Opção inválida')
+  input('Digite uma tecla para voltar ao menu principal')
+  main()
+
+def cadastrar_novo_restaurante():
+  os.system('clear')
+  print('Cadastro de novos restaurantes\n')
+  nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+  restaurantes.append(nome_do_restaurante)
+  print(f'Restaurante {nome_do_restaurante} cadastrado com sucesso')
+
+  input('\nDigite uma tecla para voltar ao menu principal')
+  main()
+
+def listar_restaurantes():
+  os.system('clear')
+  print('Listando restaurantes\n')
+  
+  for restaurante in restaurantes:
+    print(f'.{restaurante}')
+
+  input('\nDigite uma tecla para voltar ao menu principal')
+  main()
 
 def escolher_opcao():
-  opcao_escolhida = int(input('Digite a opção desejada: '))
-  print(f'Você escolheu a opção, {opcao_escolhida}')
+  try:
+    opcao_escolhida = int(input('Escolha uma opção: '))
 
-  if opcao_escolhida == 1:
-    print('Cadastrando Restaurante')
+    if opcao_escolhida == 1:
+      cadastrar_novo_restaurante()
 
-  elif opcao_escolhida == 2:
-    print('Listando Restaurante')
+    elif opcao_escolhida == 2:
+      listar_restaurantes()
 
-  elif opcao_escolhida == 3:
-    print('Ativando Restaurante')
+    elif opcao_escolhida == 3:
+      print('Ativando Restaurante')
 
-  else:
-    finalizar_app()  
+    elif opcao_escolhida == 4:
+      finalizar_app()
+
+    else:
+      opcao_invalida()  
+
+  except:
+    opcao_invalida()
 
 def main():
+  os.system('clear')
   exibir_nome_do_progrma()
   exibir_opcoes()
   escolher_opcao()
